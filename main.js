f741f27c27b1f219e7bb9a935a407e2fbd3a11e3
Leah Sanchez <leahsanchez@Leahs-MacBook-Pro.local>
2022-03-03 15:55:38 -0700

    all tests passed

--- main.js
+++ main.js
@@ -1,7 +1,71 @@
 // Your code here.
 
+function getFirstName(obj) {
+
+  return obj.firstName
+}
+
+function getLastName(obj) {
+  
+  return obj.lastName
+}
+
+function getFullName(obj) {
+  
+  return obj.firstName + " " +obj.lastName
+}
+
+function setFirstName(obj, newName) {
+  
+  obj.firstName = newName
+
+  return obj.firstName
+}
+
+function setAge(obj, newAge) {
+  
+  obj.age = newAge
+  
+  return obj.newAge
+}
 
 
+
+function giveBirthday(obj) {
+
+  if (obj["age"] === undefined) {
+    
+    return obj["age"] = 1;
+
+  } else {
+
+    
+    return obj.age ++;
+  }
+
+}
+
+function marry(obj1, obj2) {
+  
+  obj1.married = true
+  
+  obj2.married = true
+
+
+  obj2.spouseName = obj1.firstName + " " + obj1.lastName;
+  
+  obj1.spouseName = obj2.firstName + " " + obj2.lastName;
+  
+
+}
+
+function divorce(obj1, obj2) {
+  obj1.married = false
+  obj2.married = false
+
+  delete obj1.spouseName
+  delete obj2.spouseName
+}
 
 
 // Our code here. Don't touch!
